4bb425d9947e79c2f74eb8cca8d7050982a5090c
grzegorz12p <grzegorz.pytlak@student.wat.edu.pl>
2023-03-29 17:20:58 +0200

    first commit

---
+++ vue-project/src/components/WelcomeItem.vue
@@ -0,0 +1,94 @@
+<template>
+  <span v-if="element">{{ element.title  }}</span>
+</template>
+
+<script setup lang="ts">
+import router from '@/router';
+import { onBeforeMount, ref } from 'vue';
+interface Todo {
+  completed: boolean
+  id: number
+  title: string
+  userId: number
+}
+
+const element = ref<Todo>();
+
+onBeforeMount(async() => {
+  const data = await fetch(`https://jsonplaceholder.typicode.com/todos/${router.currentRoute.value.params.id}`);
+  element.value = await data.json();
+});
+</script>
+
+<style scoped>
+.item {
+  margin-top: 2rem;
+  display: flex;
+}
+
+.details {
+  flex: 1;
+  margin-left: 1rem;
+}
+
+i {
+  display: flex;
+  place-items: center;
+  place-content: center;
+  width: 32px;
+  height: 32px;
+
+  color: var(--color-text);
+}
+
+h3 {
+  font-size: 1.2rem;
+  font-weight: 500;
+  margin-bottom: 0.4rem;
+  color: var(--color-heading);
+}
+
+@media (min-width: 1024px) {
+  .item {
+    margin-top: 0;
+    padding: 0.4rem 0 1rem calc(var(--section-gap) / 2);
+  }
+
+  i {
+    top: calc(50% - 25px);
+    left: -26px;
+    position: absolute;
+    border: 1px solid var(--color-border);
+    background: var(--color-background);
+    border-radius: 8px;
+    width: 50px;
+    height: 50px;
+  }
+
+  .item:before {
+    content: ' ';
+    border-left: 1px solid var(--color-border);
+    position: absolute;
+    left: 0;
+    bottom: calc(50% + 25px);
+    height: calc(50% - 25px);
+  }
+
+  .item:after {
+    content: ' ';
+    border-left: 1px solid var(--color-border);
+    position: absolute;
+    left: 0;
+    top: calc(50% + 25px);
+    height: calc(50% - 25px);
+  }
+
+  .item:first-of-type:before {
+    display: none;
+  }
+
+  .item:last-of-type:after {
+    display: none;
+  }
+}
+</style>
---
+++ vue-project/src/views/AboutView.vue
@@ -0,0 +1,19 @@
+<template>
+ <WelcomeItem></WelcomeItem>
+</template>
+
+<script setup lang="ts">
+import WelcomeItem from '@/components/WelcomeItem.vue';
+
+</script>
+
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
